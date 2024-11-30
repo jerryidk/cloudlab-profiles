3b05f065b74ef134ffbff81f8579fd01157b46b7
jerry <jerryzhang19257@gmail.com>
2024-11-30 16:23:05 -0700

    fix

--- setup.sh
+++ setup.sh
@@ -13,10 +13,23 @@ sudo mkfs.ext4 /dev/sda4
 sudo mkdir -p ${MOUNT_DIR}
 sudo mount /dev/sda4 ${MOUNT_DIR}
 
-# install nix
-sudo mkdir /nix
-sudo mount --bind ${MOUNT_DIR}/nix /nix
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
+# set up fs for user
+sudo mkdir -p ${MOUNT_DIR}/users/${USER}
+sudo mount -o bind ${MOUNT_DIR}/users/${USER} /users/${USER}
 
-# user mount
-sudo mount ${MOUNT_DIR}/users/${USER} /users/${USER}
+# set up fs for nix
+sudo mkdir -p /nix
+sudo mkdir -p ${MOUNT_DIR}/nix
+sudo mount -o bind ${MOUNT_DIR}/nix /nix
+
+# permission
+sudo chown +R ${USER} ${MOUNT_DIR}
+
+# update apt just in case  
+sudo apt update
+
+# install nix single user
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# clone your repos here
