52f07a68136aac7d2f174de52631a5113e0eb7b8
jerry <jerryzhang19257@gmail.com>
2024-12-01 11:46:28 -0700

    final

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# sda4 is the disk. need to set this up as MOUNT_DIR 
 MOUNT_DIR=/opt/mnt
-USER=jerryidk
+USER=jerryidk # CHANGE ME
 
-# set sudo privilege
+# ensures sudo privilege
 if [[ ${SUDO_GID} == "" ]]; then
   GROUP=$(id -g -n)
 fi
@@ -13,23 +14,14 @@ sudo mkfs.ext4 /dev/sda4
 sudo mkdir -p ${MOUNT_DIR}
 sudo mount /dev/sda4 ${MOUNT_DIR}
 
-# set up fs for user
-sudo mkdir -p ${MOUNT_DIR}/users/${USER}
-#sudo mount -o bind ${MOUNT_DIR}/users/${USER} /users/${USER}
-
-# set up fs for nix
-sudo mkdir -p /nix
-sudo mkdir -p ${MOUNT_DIR}/nix
-sudo mount -o bind ${MOUNT_DIR}/nix /nix
-
 # permission
-sudo chown +R ${USER} ${MOUNT_DIR}
+sudo chown -R ${USER} ${MOUNT_DIR}
 
 # update apt just in case  
 sudo apt update
 
-# install nix single user
+# OPTIONAL install nix.
+sudo mkdir -p /nix
+sudo mkdir -p ${MOUNT_DIR}/nix
+sudo mount -o bind ${MOUNT_DIR}/nix /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
-. ~/.nix-profile/etc/profile.d/nix.sh
-
-# clone your repos here
