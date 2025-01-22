a8efa816c65db10a6327c1d300fc3f5c7dd3f366
jerry <jerryzhang19257@gmail.com>
2025-01-22 15:18:52 -0700

    save

--- setup.sh
+++ setup.sh
@@ -11,21 +11,21 @@ if [[ ${SUDO_GID} == "" ]]; then
 fi
 
 # format filesystem and mount 
-sudo mkfs.ext4 /dev/sda4
-sudo mkdir -p ${MOUNT_DIR}
-sudo mount /dev/sda4 ${MOUNT_DIR}
-
-# permission
-sudo chown -R ${USER} ${MOUNT_DIR}
-
-# update apt just in case  
-sudo apt update
-
-# OPTIONAL install nix.
-sudo mkdir -p /nix
-sudo mkdir -p ${MOUNT_DIR}/nix
-sudo mount -o bind ${MOUNT_DIR}/nix /nix
-yes | sh <(curl -L https://nixos.org/nix/install) --daemon
+#sudo mkfs.ext4 /dev/sda4
+#sudo mkdir -p ${MOUNT_DIR}
+#sudo mount /dev/sda4 ${MOUNT_DIR}
+#
+## permission
+#sudo chown -R ${USER} ${MOUNT_DIR}
+#
+## update apt just in case  
+#sudo apt update
+#
+## OPTIONAL install nix.
+#sudo mkdir -p /nix
+#sudo mkdir -p ${MOUNT_DIR}/nix
+#sudo mount -o bind ${MOUNT_DIR}/nix /nix
+#yes | sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # OPTIONAL bind apt directory so it doesn't run out space
 #sudo mount --bind ${MOUNT_DIR} /var/lib/dpkg
