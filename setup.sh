5a1ce6c63dba3e243bb07869b09dfd81b08c8995
jerry <jerryzhang19257@gmail.com>
2025-01-22 15:51:49 -0700

    save

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 # sda4 has more storage, need to set this up as MOUNT_DIR 
 MOUNT_DIR=/opt/mnt
 USER=jerryidk # CHANGE ME
+DISK=/dev/nvme0n1p4 # CHANGE ME
 
 # ensures sudo privilege
 if [[ ${SUDO_GID} == "" ]]; then
@@ -11,23 +12,23 @@ if [[ ${SUDO_GID} == "" ]]; then
 fi
 
 # format filesystem and mount 
-#sudo mkfs.ext4 /dev/sda4
-#sudo mkdir -p ${MOUNT_DIR}
-#sudo mount /dev/sda4 ${MOUNT_DIR}
-#
-## permission
-#sudo chown -R ${USER} ${MOUNT_DIR}
-#
-## update apt just in case  
-#sudo apt update
-#
-## OPTIONAL install nix.
-#sudo mkdir -p /nix
-#sudo mkdir -p ${MOUNT_DIR}/nix
-#sudo mount -o bind ${MOUNT_DIR}/nix /nix
-#yes | sh <(curl -L https://nixos.org/nix/install) --daemon
+sudo mkfs.ext4 ${DISK}
+sudo mkdir -p ${MOUNT_DIR}
+sudo mount ${DISK} ${MOUNT_DIR}
+
+# permission
+sudo chown -R ${USER} ${MOUNT_DIR}
+
+# update apt just in case  
+sudo apt update
+
+# OPTIONAL install nix.
+sudo mkdir -p /nix
+sudo mkdir -p ${MOUNT_DIR}/nix
+sudo mount -o bind ${MOUNT_DIR}/nix /nix
+yes | sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # OPTIONAL bind apt directory so it doesn't run out space
-#sudo mount --bind ${MOUNT_DIR} /var/lib/dpkg
-#sudo mount --bind ${MOUNT_DIR} /var/lib/apt
-#sudo mount --bind ${MOUNT_DIR} /usr/lib/apt
+# sudo mount --bind ${MOUNT_DIR} /var/lib/dpkg
+# sudo mount --bind ${MOUNT_DIR} /var/lib/apt
+# sudo mount --bind ${MOUNT_DIR} /usr/lib/apt
