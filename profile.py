6daa4e27f0ffcf9cb8414addad79329cc0ec1f39
jerry <jerryzhang19257@gmail.com>
2026-03-19 14:26:55 -0600

    test

--- profile.py
+++ profile.py
@@ -1,41 +1,29 @@
 """
-Jerry's Custom cloudlab set up 
+Jerry's Custom cloudlab set up
 
 Instructions:
 Nothing to do here, look into .sh if you would like
-"""	  
+"""
 
 import geni.portal as portal
 import geni.rspec.pg as pg
 
 pc = portal.Context()
 
-pc.defineParameter(
-    "hw_type", 
-    "Hardware Type", 
-    portal.ParameterType.NODETYPE, 
-    "", # Default is empty, allowing the user to pick, or the mapper to choose
-    longDescription="Select a physical node type. The CloudLab UI will automatically populate this list based on the cluster's current inventory."
-)
+pc.defineParameter("hw_type", "Hardware Type", portal.ParameterType.NODETYPE, "")
 
-pc.defineParameter(
-    "os_image",
-    "Operating System Image",
-    portal.ParameterType.IMAGE,
-    "",
-    longDescription="Select the base OS image. Ubuntu 24.04 (Kernel 6.8+) is the default."
-)
+pc.defineParameter("os_image", "Operating System Image", portal.ParameterType.IMAGE, "")
 
 params = pc.bindParameters()
 request = pc.makeRequestRSpec()
 
-node = request.RawPC('dramhit')
+node = request.RawPC("dramhit")
 
-# 3. Apply the dynamic parameters
 if params.hw_type != "":
     node.hardware_type = params.hw_type
 
-node.disk_image = params.os_image
+if params.os_image != "":
+    node.disk_image = params.os_image
 
 node.addService(pg.Execute(shell="bash", command="/local/repository/setup.sh"))
 
