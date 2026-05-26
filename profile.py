c2d23a6f908119f9856fef888ec7670a8515a581
jerry <jerryzhang19257@gmail.com>
2026-05-26 14:13:27 -0600

    all

--- profile.py
+++ profile.py
@@ -1,8 +1,8 @@
 """
-Jerry's Custom cloudlab set up
+Two machine of same node type connected by ethernet
 
 Instructions:
-Nothing to do here, look into .sh if you would like
+ 
 """
 
 import geni.portal as portal
@@ -22,18 +22,51 @@ pc.defineParameter(
 params = pc.bindParameters()
 request = pc.makeRequestRSpec()
 
-node = request.RawPC("dramhit")
+# ==========================================
+# Setup Node 1
+# ==========================================
+node1 = request.RawPC("node1")
 
 if params.hw_type != "":
-    node.hardware_type = params.hw_type
+    node1.hardware_type = params.hw_type
 
 if params.os_image != "":
-    node.disk_image = params.os_image
+    node1.disk_image = params.os_image
 
-bs = node.Blockstore("bs", "/opt")
-# "Use all remaining unallocated disk space on this physical machine"
-bs.size = "0"
+bs1 = node1.Blockstore("bs1", "/opt")
+bs1.size = "0" # Use all remaining unallocated disk space
+node1.addService(pg.Execute(shell="bash", command="/local/repository/setup.sh"))
 
-node.addService(pg.Execute(shell="bash", command="/local/repository/setup.sh"))
+# Create an Ethernet interface for Node 1
+iface1 = node1.addInterface("eth1")
+
+
+# ==========================================
+# Setup Node 2
+# ==========================================
+node2 = request.RawPC("node2")
+
+if params.hw_type != "":
+    node2.hardware_type = params.hw_type
+
+if params.os_image != "":
+    node2.disk_image = params.os_image
+
+# Note: Blockstore names must be unique across the experiment, so we use "bs2"
+bs2 = node2.Blockstore("bs2", "/opt")
+bs2.size = "0"
+node2.addService(pg.Execute(shell="bash", command="/local/repository/setup.sh"))
+
+# Create an Ethernet interface for Node 2
+iface2 = node2.addInterface("eth1")
+
+
+# ==========================================
+# Setup the Ethernet Link
+# ==========================================
+# Create a point-to-point link connecting the two interfaces
+link = request.Link("lan")
+link.addInterface(iface1)
+link.addInterface(iface2)
 
 pc.printRequestRSpec(request)
--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 MOUNT_DIR="/opt"
+
+# Installing nix
 sudo mkdir -p /nix "$MOUNT_DIR/nix"
 sudo mount --bind "$MOUNT_DIR/nix" /nix
 yes | sh <(curl -L https://nixos.org/nix/install) --daemon
+
+sudo apt update
+sudo apt install gh cmake-curses-gui
+
